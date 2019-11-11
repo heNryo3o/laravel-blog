8cb9df3c33c7c7ad50fe08c3b533fca2aed7102f
henryo3o <18661139072@163.com>
2019-11-11 11:23:45 +0800

    1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-  <meta name="keyword" content="@yield('keyword', sysConfig('SITE_KEYWORD'))"/>
+  <meta name="keywords" content="@yield('keyword', sysConfig('SITE_KEYWORD'))"/>
   <meta name="description" content="@yield('description', sysConfig('SITE_DESCRIPTION'))"/>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="author" content="Flex">
